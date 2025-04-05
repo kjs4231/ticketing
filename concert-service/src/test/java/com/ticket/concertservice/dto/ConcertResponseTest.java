ea2f1adb539c3991d4cd12671d91a895d3e4ba0b
kjs4231 <kjs4231@naver.com>
2025-04-05 13:40:27 +0900

    refactor: concert 테스트 커버리지 향상
    concert 테스트 커버리지 향상

--- concert-service/src/test/java/com/ticket/concertservice/dto/ConcertResponseTest.java
+++ concert-service/src/test/java/com/ticket/concertservice/dto/ConcertResponseTest.java
@@ -11,7 +11,7 @@ class ConcertResponseTest {
 
     @Test
     void testConcertResponseFromConcert() {
-        // given
+        // given : Concert 객체 생성
         Concert concert = Concert.builder()
                 .concertId(1L)
                 .title("콘서트 제목")
@@ -21,10 +21,10 @@ class ConcertResponseTest {
                 .capacity(100L)
                 .build();
 
-        // when
+        // when : ConcertResponse.from() 을 통해 변환
         ConcertResponse response = ConcertResponse.from(concert);
 
-        // then
+        // then : 각 필드가 올바르게 매핑되었는지 검증
         assertNotNull(response);
         assertEquals(concert.getConcertId(), response.getConcertId());
         assertEquals(concert.getTitle(), response.getTitle());
@@ -36,7 +36,7 @@ class ConcertResponseTest {
 
     @Test
     void testConcertResponseWithNullValues() {
-        // given
+        // given : 모든 값이 null인 Concert 객체 생성
         Concert concert = Concert.builder()
                 .concertId(null)
                 .title(null)
@@ -46,10 +46,10 @@ class ConcertResponseTest {
                 .capacity(null)
                 .build();
 
-        // when
+        // when : 변환 호출
         ConcertResponse response = ConcertResponse.from(concert);
 
-        // then
+        // then : 결과 필드 모두 null 확인
         assertNotNull(response);
         assertNull(response.getConcertId());
         assertNull(response.getTitle());
@@ -61,7 +61,7 @@ class ConcertResponseTest {
 
     @Test
     void testConcertResponseWithDifferentValues() {
-        // given
+        // given : 다른 값들을 가진 Concert 생성
         Concert concert = Concert.builder()
                 .concertId(2L)
                 .title("다른 콘서트 제목")
@@ -74,7 +74,7 @@ class ConcertResponseTest {
         // when
         ConcertResponse response = ConcertResponse.from(concert);
 
-        // then
+        // then : 각 필드의 값이 올바른지 확인
         assertNotNull(response);
         assertEquals(concert.getConcertId(), response.getConcertId());
         assertEquals(concert.getTitle(), response.getTitle());
@@ -82,5 +82,28 @@ class ConcertResponseTest {
         assertEquals(concert.getDateTime(), response.getDateTime());
         assertEquals(concert.getUserEmail(), response.getUserEmail());
         assertEquals(concert.getCapacity(), response.getCapacity());
+    }
+
+    @Test
+    void testConcertResponseBuilderDirectly() {
+        // 이번 테스트는 Lombok의 @Builder와 @Getter가 제대로 동작하는지 직접 확인하기 위한 것입니다.
+        LocalDateTime dateTime = LocalDateTime.of(2025, 4, 5, 12, 0);
+        ConcertResponse response = ConcertResponse.builder()
+                .concertId(3L)
+                .title("빌더 테스트 제목")
+                .description("빌더 테스트 설명")
+                .dateTime(dateTime)
+                .userEmail("builder@test.com")
+                .capacity(300L)
+                .build();
+
+        // then : builder로 생성한 값이 getter를 통해서 올바르게 리턴되는지 확인
+        assertNotNull(response);
+        assertEquals(3L, response.getConcertId());
+        assertEquals("빌더 테스트 제목", response.getTitle());
+        assertEquals("빌더 테스트 설명", response.getDescription());
+        assertEquals(dateTime, response.getDateTime());
+        assertEquals("builder@test.com", response.getUserEmail());
+        assertEquals(300L, response.getCapacity());
     }
 }
