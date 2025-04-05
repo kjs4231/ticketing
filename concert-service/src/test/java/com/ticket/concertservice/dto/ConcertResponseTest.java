bb7861fbc24e4f0b8c9afded727c9f27536014ac
kjs4231 <kjs4231@naver.com>
2025-04-05 13:34:49 +0900

    refactor: concert 테스트 커버리지 향상
    concert 테스트 커버리지 향상

--- concert-service/src/test/java/com/ticket/concertservice/dto/ConcertResponseTest.java
+++ concert-service/src/test/java/com/ticket/concertservice/dto/ConcertResponseTest.java
@@ -33,4 +33,54 @@ class ConcertResponseTest {
         assertEquals(concert.getUserEmail(), response.getUserEmail());
         assertEquals(concert.getCapacity(), response.getCapacity());
     }
+
+    @Test
+    void testConcertResponseWithNullValues() {
+        // given
+        Concert concert = Concert.builder()
+                .concertId(null)
+                .title(null)
+                .description(null)
+                .dateTime(null)
+                .userEmail(null)
+                .capacity(null)
+                .build();
+
+        // when
+        ConcertResponse response = ConcertResponse.from(concert);
+
+        // then
+        assertNotNull(response);
+        assertNull(response.getConcertId());
+        assertNull(response.getTitle());
+        assertNull(response.getDescription());
+        assertNull(response.getDateTime());
+        assertNull(response.getUserEmail());
+        assertNull(response.getCapacity());
+    }
+
+    @Test
+    void testConcertResponseWithDifferentValues() {
+        // given
+        Concert concert = Concert.builder()
+                .concertId(2L)
+                .title("다른 콘서트 제목")
+                .description("다른 콘서트 설명")
+                .dateTime(LocalDateTime.now().plusDays(14))
+                .userEmail("different@test.com")
+                .capacity(200L)
+                .build();
+
+        // when
+        ConcertResponse response = ConcertResponse.from(concert);
+
+        // then
+        assertNotNull(response);
+        assertEquals(concert.getConcertId(), response.getConcertId());
+        assertEquals(concert.getTitle(), response.getTitle());
+        assertEquals(concert.getDescription(), response.getDescription());
+        assertEquals(concert.getDateTime(), response.getDateTime());
+        assertEquals(concert.getUserEmail(), response.getUserEmail());
+        assertEquals(concert.getCapacity(), response.getCapacity());
+    }
 }
