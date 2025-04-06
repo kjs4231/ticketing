70c35336589beb6439864bdcd4c731d3500b39a0
kjs4231 <kjs4231@naver.com>
2025-04-06 17:11:16 +0900

    rediis 설정 추가

--- reservation-service/src/test/java/com/ticket/reservationservice/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/ticket/reservationservice/ReservationServiceApplicationTests.java
@@ -1,10 +1,26 @@
 package com.ticket.reservationservice;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class ReservationServiceApplicationTests {
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public RedissonClient redissonClient() {
+            Config config = new Config();
+            config.useSingleServer()
+                    .setAddress("redis://localhost:6379");
+            return Redisson.create(config);
+        }
+    }
 
     @Test
     void contextLoads() {
